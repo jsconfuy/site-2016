4a006cb22b53cf742cbf717794099ca24cf5ee3f
Alejo Rodriguez <a.rodsott@gmail.com>
2015-11-18 16:56:39 -0300

    update scss file to comply with linters

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,37 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jshintReporter = require('jshint-stylish');
-var watch = require('gulp-watch');
+var scsslint = require('gulp-scss-lint');
+var eslint = require('gulp-eslint');
+var jscs = require('gulp-jscs');
 
 /*
  * Create variables for our project paths so we can change in one place
  */
 var paths = {
-  'src': ['./models/**/*.js', './routes/**/*.js', 'keystone.js', 'package.json']
+  jsSrc: ['./models/**/*.js', './routes/**/*.js', 'keystone.js', 'package.json'],
+  scssSrc: ['./public/styles/**/*.scss', '!./public/styles/bootstrap/**/*.scss', '!./public/styles/font-awesome/**/*.scss']
 };
 
-// gulp lint
-gulp.task('lint', function () {
-  gulp.src(paths.src)
-    .pipe(jshint())
-    .pipe(jshint.reporter(jshintReporter));
+gulp.task('lint-js', function () {
+  gulp.src(paths.jsSrc)
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
-// gulp watcher for lint
-gulp.task('watch:lint', function () {
-  gulp.src(paths.src)
-    .pipe(watch(paths.src))
-    .pipe(jshint())
-    .pipe(jshint.reporter(jshintReporter));
+gulp.task('lint-scss', function () {
+  gulp.src(paths.scssSrc)
+    .pipe(scsslint({
+      config: './.scss-lint.yml'
+    }));
+});
+
+gulp.task('codestyle', function () {
+  gulp.src(paths.jsSrc)
+    .pipe(jscs({
+      configPath: './.jscsrc'
+    }))
+    .pipe(jscs.reporter());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.jsSrc.concat(paths.scssSrc), ['lint-js', 'lint-scss', 'codestyle']);
 });
