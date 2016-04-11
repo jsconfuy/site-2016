0dbcb657f3701f88978c28eabb2651abe72badd6
Pablo Ricco <pricco@gmail.com>
2016-04-11 20:27:39 -0300

    Fix print

--- routes/views/print.js
+++ routes/views/print.js
@@ -10,12 +10,18 @@ exports = module.exports = function (req, res) {
   if (/-/.test(reference)) {
     Attendee.model.findOne({reference: reference}).populate('order').exec(function (err, attendee) {
       if (err) { return res.notFound(); }
-      Order.model.findOne({_id: attendee.order._id}).populate('ticket discount').exec(function (err, order) {
-        if (err) { return res.notFound(); }
-        locals.order = order;
+      if (attendee.order) {
+        Order.model.findOne({_id: attendee.order._id}).populate('ticket discount').exec(function (err, order) {
+          if (err) { return res.notFound(); }
+          locals.order = order;
+          locals.attendees = [attendee];
+          view.render('print');
+        });
+      } else {
+        locals.order = null;
         locals.attendees = [attendee];
         view.render('print');
-      });
+      }
     });
   } else {
     Order.model.findOne({reference: reference}).populate('ticket discount').exec(function (err, order) {
