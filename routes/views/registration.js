d5990560fabbcf9c38ec541e92e194a307363a3e
Pablo Ricco <pricco@gmail.com>
2016-04-11 22:02:29 -0300

    Fix registration.

--- routes/views/registration.js
+++ routes/views/registration.js
@@ -2,27 +2,42 @@ var keystone = require('keystone');
 var Attendee = keystone.list('Attendee');
 
 exports = module.exports = function (req, res) {
+  if (!req.user) {
+    res.redirect('/keystone/signin?from=/registration');
+    return;
+  }
+
   var view = new keystone.View(req, res);
   var locals = res.locals;
 
   var reference = (req.query.ref || '').toUpperCase();
-  Attendee.model.findOne({reference: reference}).populate('order').exec(function (err, attendee) {
-    locals.reference = reference;
-    if (err || !attendee) {
-      locals.attendee = null;
-      locals.status = 404;
+  var text = (req.query.text || '');
+  if (text) {
+    Attendee.model.find({'$or': [{'name': RegExp(text, 'i')}, {'email': RegExp(text, 'i')}]}).exec(function (err, attendees) {
+      locals.text = text;
+      locals.attendees = attendees;
+      locals.status = 302;
       view.render('registration');
-    } else if (attendee.registered) {
-      locals.attendee = attendee;
-      locals.status = 409;
-      view.render('registration');
-    } else {
-      attendee.registered = Date.now();
-      attendee.save(function (err) {
-        locals.attendee = attendee;
-        locals.status = 200;
+    });
+  } else {
+    Attendee.model.findOne({reference: reference}).populate('order').exec(function (err, attendee) {
+      locals.reference = reference;
+      if (err || !attendee) {
+        locals.attendee = null;
+        locals.status = 404;
         view.render('registration');
-      });
-    }
-  });
+      } else if (attendee.registered) {
+        locals.attendee = attendee;
+        locals.status = 409;
+        view.render('registration');
+      } else {
+        attendee.registered = Date.now();
+        attendee.save(function (err) {
+          locals.attendee = attendee;
+          locals.status = 200;
+          view.render('registration');
+        });
+      }
+    });
+  }
 };
